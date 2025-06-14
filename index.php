a119a18daa4d6caa6925416d3798fb6f464853c6
dana2096 <denaavv@gmail.com>
2025-06-14 19:39:44 +0300

    الواجهة الاولى

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>شركة الإسراءلتأجير سيارات</title>
+  <title>شركة الإسراء لتأجير سيارات</title>
   <link rel="stylesheet" href="style.css" />
 </head>
 <body>
   <header class="navbar">
-    <img src="logo.png" alt="https://th.bing.com/th/id/OIP.koF0FD7H5shHroX6z3p-cAHaKf?rs=1&pid=ImgDetMain" class="logo" />
+    
+    <img src="https://th.bing.com/th/id/OIP.koF0FD7H5shHroX6z3p-cAHaKf?rs=1&pid=ImgDetMain&cb=idpwebpc1" alt="Company Logo" class="logo" />
+    
     <nav>
       <ul>
         <li><a href="#">Home</a></li>
@@ -34,7 +36,7 @@
         </div>
       </div>
       <div class="image-area">
-        <img src="whitecar.png" alt="Car Image" />
+        <img src="https://file.kelleybluebookimages.com/kbb/base/evox/CP/15070/2022-Kia-Sportage-front_15070_032_1865x880_SWP_cropped.png?interpolation=high-quality&downsize=*:150" alt="Car Image" style="max-width: 100%; height: auto;" />
       </div>
     </div>
   </main>
