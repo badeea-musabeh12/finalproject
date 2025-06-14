aedb458354f46e39779ef787c1abea3ddf1f424c
dana2096 <denaavv@gmail.com>
2025-06-14 19:22:49 +0300

    صفحة تسجيل

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>شركة الإسراءلتأجير سيارات</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <header class="navbar">
+    <img src="logo.png" alt="https://th.bing.com/th/id/OIP.koF0FD7H5shHroX6z3p-cAHaKf?rs=1&pid=ImgDetMain" class="logo" />
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Cars</a></li>
+        <li><a href="#">Location</a></li>
+        <li><a href="#">Contact us</a></li>
+      </ul>
+    </nav>
+    <button class="login-btn">Log in</button>
+  </header>
+
+  <main class="home-section">
+    <div class="home-content">
+      <div class="text-area">
+        <h1>EASY AND FAST WAY<br>TO RENT YOUR CAR</h1>
+        <p>
+          Whether you're planning a weekend getaway or a cross-country adventure, we’ve got you covered. 
+          With our wide selection of vehicles and convenient booking system, renting a car has never been this effortless.
+        </p>
+        <div class="button-group">
+          <a href="#" class="btn blue">Cars</a>
+          <a href="#" class="btn">Contact us</a>
+        </div>
+      </div>
+      <div class="image-area">
+        <img src="whitecar.png" alt="Car Image" />
+      </div>
+    </div>
+  </main>
+</body>
+</html>
